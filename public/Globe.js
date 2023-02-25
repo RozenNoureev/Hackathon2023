ee6ad9726f929277159a7eb560efd9600e0dcf07
RozenNoureev <rozennoureev@gmail.com>
2023-02-25 12:45:27 -0600

    globe

---
+++ public/Globe.js
@@ -0,0 +1,18 @@
+    // Gen random data
+    const N = 300;
+    const gData = [...Array(N).keys()].map(() => ({
+      lat: (Math.random() - 0.5) * 180,
+      lng: (Math.random() - 0.5) * 360,
+      size: Math.random() / 3,
+      color: ['purple', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+    }));
+  
+    ReactDOM.render(
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        pointsData={gData}
+        pointAltitude="size"
+        pointColor="color"
+      />,
+      document.getElementById('globeViz')
+    );
